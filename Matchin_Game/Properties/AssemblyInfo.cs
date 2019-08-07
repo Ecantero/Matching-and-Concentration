6fce5fe43f9ade58eb8df141fa3ac2ac90a53527
Mariela Cisneros <47341516+MarielaCisneros@users.noreply.github.com>
2019-08-07 17:35:52 -0600

    New solution
    created a new solution

--- Matchin_Game/Properties/AssemblyInfo.cs
+++ Matchin_Game/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Matching and Concentration")]
+[assembly: AssemblyTitle("Matchin_Game")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Matching and Concentration")]
+[assembly: AssemblyProduct("Matchin_Game")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
