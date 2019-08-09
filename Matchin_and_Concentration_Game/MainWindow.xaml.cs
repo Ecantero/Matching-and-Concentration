9772c0570b20765d9ebbb60dfe0d215aa7b7de20
Mariela Cisneros <47341516+MarielaCisneros@users.noreply.github.com>
2019-08-09 14:28:09 -0600

    New Project

---
+++ Matchin_and_Concentration_Game/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using m.Difficulty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace m
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Easy_Click(object sender, RoutedEventArgs e)
+        {
+            Easy_Class.Run();
+        }
+
+        private void Medium_Click(object sender, RoutedEventArgs e)
+        {
+            Medium_Class.Run();
+        }
+
+        private void Hard_Click(object sender, RoutedEventArgs e)
+        {
+            Hard_Class.Run();
+        }
+    }
+}
