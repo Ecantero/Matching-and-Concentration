3b08ba7fc9025faa2545b4bc099f8a5d83ce5666
Mariela Cisneros <47341516+MarielaCisneros@users.noreply.github.com>
2019-08-09 13:38:35 -0600

    m

---
+++ Matchin_and_Concentration_Game/Difficulty_classes/Medium_Class.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Matchin_Game.Difficulty_classes
+{
+    class Medium_Class
+    {
+        private static bool allowClick = false;
+        private static PictureBox firtGuess;
+        private static Random rng = new Random();
+        private static Timer clickTimer = new Timer();
+        private static int time = 360;
+        private static DispatcherTimer timer = new DispatcherTimer();
+
+        private static IEnumerable<Image> images
+        {
+
+            get
+            {
+                return new Image[]
+                {
+
+                };
+            }
+        }
+
+        private static void time_medum()
+        {
+            timer.Start();
+            timer.Tick += delegate
+            {
+                time--;
+                if (time < 0)
+                {
+                    timer.Stop();
+                }
+            };
+        }
+    }
+}
--- Matchin_and_Concentration_Game/Matching_Game.xaml.cs
+++ Matchin_and_Concentration_Game/Matching_Game.xaml.cs
@@ -22,31 +22,18 @@ namespace Matchin_Game
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public partial class MainPage : Page
     {
-
-        bool allowClick = false;
-        ImageSource firtGuess;
-        Random rng = new Random();
-        private static int time = 300;
-        private static int time2 = 360;
-        private static int time3 = 600;
-        private static Timer timer = new Timer { Interval = 1000 }; 
 
 
         public MainPage()
         {
             this.InitializeComponent();
 
-            Image img = new Image();
-            img.Source = new BitmapImage(new Uri("http://www.contoso.com/images/logo.png"));
         }
-
 
         private static IEnumerable<Image> images
         {
-
-
             get
             {
                 return new Image[]
@@ -55,6 +42,7 @@ namespace Matchin_Game
                 };
             }
         }
+
         private void Easy_Click(object sender, RoutedEventArgs e)
         {
 
@@ -70,70 +58,5 @@ namespace Matchin_Game
 
         }
 
-        private void Easy_Game()
-        {
-            //Time_Easy();
-        }
-
-        private void Medium_Game()
-        {
-            //Time_Medum();
-        }
-
-        private void Hard_Game()
-        {
-            //Time_Hard();
-        }
-
-        //private static void Time_Easy()
-        //{
-        //    timer.Start();
-        //    timer.Tick += delegate
-        //    {
-        //        time--;
-        //        if(time < 0)
-        //        {
-        //            timer.Stop();
-        //            MessageBox.Show("Out of time");
-        //        }
-
-        //        var ssTime = TimeSpan.FromSeconds(time);
-        //        lblTime.Text = "00: " + time.ToString();
-        //    };   
-        //}
-
-        //private static void Time_Medum()
-        //{
-        //    timer.Start();
-        //    timer.Tick += delegate
-        //    {
-        //        time2--;
-        //        if (time2 < 0)
-        //        {
-        //            timer.Stop();
-        //            MessageBox.Show("Out of time");
-        //        }
-
-        //        var ssTime = TimeSpan.FromSeconds(time2);
-        //        lblTime.Text = "00: " + time2.ToString();
-        //    };
-        //}
-
-        //private static void Time_Hard()
-        //{
-        //    timer.Start();
-        //    timer.Tick += delegate
-        //    {
-        //        time3--;
-        //        if (time3 < 0)
-        //        {
-        //            timer.Stop();
-        //            MessageBox.Show("Out of time");
-        //        }
-
-        //        var ssTime = TimeSpan.FromSeconds(time3);
-        //        lblTime.Text = "00: " + time3.ToString();
-        //    };
-        //}
     }
 }
