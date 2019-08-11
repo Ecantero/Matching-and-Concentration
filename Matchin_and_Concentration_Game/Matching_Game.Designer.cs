42bd8280d3f09c7a88d5083ebb5e5b651fbfd249
Mariela Cisneros <47341516+MarielaCisneros@users.noreply.github.com>
2019-08-11 17:10:30 -0600

    Name

--- Matchin_and_Concentration_Game/Matching_Game.Designer.cs
+++ Matchin_and_Concentration_Game/Matching_Game.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class Matching_Game
     {
         /// <summary>
         /// Required designer variable.
--- Matchin_and_Concentration_Game/Matching_Game.cs
+++ Matchin_and_Concentration_Game/Matching_Game.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Matching_Game : Form
     {
-        public Form1()
+        public Matching_Game()
         {
             InitializeComponent();
         }
