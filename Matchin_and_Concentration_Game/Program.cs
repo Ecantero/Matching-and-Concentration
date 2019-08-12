13ddf757236eec9aed14ba119d137f05bba57381
Mariela Cisneros <47341516+MarielaCisneros@users.noreply.github.com>
2019-08-12 13:47:11 -0600

    m

--- Matchin_and_Concentration_Game/Program.cs
+++ Matchin_and_Concentration_Game/Program.cs
@@ -16,8 +16,11 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Matching_Game());
-            
+            Application.Run(new Easy_Match());
+            Application.Run(new Medium_Match());
+            Application.Run(new Hard_Match());
+
+
         }
     }
 }
