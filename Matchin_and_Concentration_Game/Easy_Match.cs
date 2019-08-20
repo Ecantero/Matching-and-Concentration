57fa8e6f31b443fc8f0a11d14fd8cef078ec6895
Mariela Cisneros <47341516+MarielaCisneros@users.noreply.github.com>
2019-08-20 13:45:08 -0600

    players

--- Matchin_and_Concentration_Game/Easy_Match.cs
+++ Matchin_and_Concentration_Game/Easy_Match.cs
@@ -48,34 +48,35 @@ namespace MatchingGame
             InitializeComponent();
         }
 
-        private int NumOfPlayers()
-        {
-            int numberPlayer = 0;
-            string playerName = "";
+        //private int NumOfPlayers()
+        //{
+        //    int numberPlayer = 0;
+        //    string playerName = "";
 
-            while (numberPlayer <= 0)
-            {
-                try
-                {
-                    numberPlayer = Int32.Parse(TextBox.ReflectMessage(2, "Please enter the number of player: ", "Please enter the number of player: "));
-                }
-                catch (FormatException)
-                {
-                    numberPlayer = 0;
-                }
+        //    while (numberPlayer <= 0)
+        //    {
+        //        try
+        //        {
+        //            numberPlayer = Int32.Parse(TextBox.("Please enter the number of player: "));
+        //        }
+        //        catch (FormatException)
+        //        {
+        //            numberPlayer = 0;
+        //        }
 
-                for (int i = 0; i < numberPlayer; i++)
-                {
-                    playerName = MessageBox.Show("Please enter the player name: ", "Name", );
+        //        for (int i = 0; i < numberPlayer; i++)
+        //        {
+        //            playerName = MessageBox.Show("Please enter the player name: ", "Name", );
 
-                    if (string.IsNullOrEmpty(playerName))
-                    {
-                        playerName = "Player " + i;
-                    }
-                }
-            }
-            return numberPlayer;
-        }
+        //            if (string.IsNullOrEmpty(playerName))
+        //            {
+        //                scores++;
+        //                score .Text= "Player " + i + " matchings: " + scores.ToString();
+        //            }
+        //        }
+        //    }
+        //    return numberPlayer;
+        //}
 
         private void Time()
         {
