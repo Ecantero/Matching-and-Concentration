2dd5519bcf5b91b84aa22fe334d7ab0bc293824b
Mariela Cisneros <47341516+MarielaCisneros@users.noreply.github.com>
2019-08-20 15:48:31 -0600

    menu

---
+++ Matchin_and_Concentration_Game/MatchingGame_Difficulty.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MatchingGame
+{
+    /// <summary>
+    /// Interaction logic for MatchingGame_Difficulty.xaml
+    /// </summary>
+    public partial class MatchingGame_Difficulty : Window
+    {
+        public MatchingGame_Difficulty()
+        {
+            InitializeComponent();
+        }
+
+        private void Easy_Click(object sender, RoutedEventArgs e)
+        {
+            Easy_Match easy_Match = new Easy_Match();
+            easy_Match.Show();
+        }
+
+        private void Medium_Click(object sender, RoutedEventArgs e)
+        {
+            Medium_Match medium_Match = new Medium_Match();
+            medium_Match.Show();
+        }
+
+        private void Hard_Click(object sender, RoutedEventArgs e)
+        {
+            Hard_Match hard_Match = new Hard_Match();
+            hard_Match.Show();
+        }
+
+        private void Extra_Click(object sender, RoutedEventArgs e)
+        {
+            Extra extra = new Extra();
+            extra.Show();
+        }
+    }
+}
