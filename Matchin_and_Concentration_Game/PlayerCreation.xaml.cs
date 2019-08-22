80a7d305b0a28e27daf47ccb8670e4a9a36a09c4
Samuel Watchman <33162082+samwat2@users.noreply.github.com>
2019-08-22 14:11:37 -0600

    player creation

--- Matchin_and_Concentration_Game/PlayerCreation.xaml.cs
+++ Matchin_and_Concentration_Game/PlayerCreation.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 namespace MatchingGame
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for PlayerCreation.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class PlayerCreation : Window
     {
-        public Window1()
+        public PlayerCreation()
         {
             InitializeComponent();
         }
